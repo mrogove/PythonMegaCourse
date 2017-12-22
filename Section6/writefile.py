9b36061e55b9604422ce5a1d7fbaa6b6d9688a1f
mrogove <mrogove@gmail.com>
2017-12-22 13:20:59 -0500

    adjusting write mode to "w" instead of "a"

--- Section6/writefile.py
+++ Section6/writefile.py
@@ -1,7 +1,7 @@
 #write list to file:
 numbers = [1,2,3]
 
-numberfile = open("numberlist.txt","a")
+numberfile = open("numberlist.txt","w")
 for idx in numbers:
     numberfile.write(str(idx)+"\n")
 
