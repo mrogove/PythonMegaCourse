8de5691fc7bc260c2fe6510eb69fec4aabeabb1f
mrogove <mrogove@gmail.com>
2018-01-04 18:59:33 -0500

    more elegant way to call strftime; removes two casts, adds extension inside the function.

--- Section7/simpleCreateFileUsingDatetime.py
+++ Section7/simpleCreateFileUsingDatetime.py
@@ -5,10 +5,10 @@ Use script to create empty file using now()
 
 filename = datetime.datetime.now()
 #use strftime
-filename = filename.strftime("%Y-%m-%d-%H-%M")
+
 #function to create empty file
 def create_file():
-    with open(str(filename),"w") as file: #must cast as str
+    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file: #must cast as str
         file.write("")
 
 create_file()
