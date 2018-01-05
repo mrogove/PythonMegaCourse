4f7ccb21c59306452ede9c2b8e5e628583c1a6d6
mrogove <mrogove@gmail.com>
2018-01-04 19:06:57 -0500

    Adding in functionality to create a dummy log for "tomorrow." Now, I can't think of a usecase, except for using

--- Section7/simpleCreateFileUsingDatetime.py
+++ Section7/simpleCreateFileUsingDatetime.py
@@ -2,13 +2,25 @@ import datetime
 """
 Use script to create empty file using now()
 """
-
-filename = datetime.datetime.now()
+#variable for now time
+now = datetime.datetime.now()
 #use strftime
 
 #function to create empty file
 def create_file():
-    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file: #must cast as str
+    with open(now.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file:
+        file.write("")
+#create an identical dummy log for "tomorrow"
+def create_tomorrow_file():
+    now = datetime.datetime.now()
+    after = now + datetime.timedelta(days=1)
+
+    with open(after.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file:
         file.write("")
 
 create_file()
+create_tomorrow_file()
+
+#note that this does not give exact time differential
+#this is because of calling datetime.datetime.now() twice
+#rather than reusing the initial call. Leaving as example.
