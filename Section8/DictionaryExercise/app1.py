4ab8909d1cec95b07cd50fcec409a6196a9e93e4
mrogove <mrogove@gmail.com>
2018-01-06 23:53:12 -0500

    Code copied from follow-along in lecture, with comments. Json, difflib, keys of dict.

---
+++ Section8/DictionaryExercise/app1.py
@@ -0,0 +1,38 @@
+"""
+Section 8 exercise:
+Use json and difflib libs to find if words entered by user
+    are present in a dictionary data file presented by course.
+    Followed along.
+
+Searches keys of dict. Python easily loads json to dict
+"""
+
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json")) #dictionary
+
+def translate(w):
+    w = w.lower() #make input insensitive; lookup data is lowercase
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0: #account for near-misses
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0]) #is this a duplicate call?
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist in our dataset. Please double check it." #in lieu of getting to bottom of loop
+        else:
+            return "We didn't understand your entry." #out of binary Y/N
+    else:
+        return "The word doesn't exist in our dataset. Please double check it."
+
+word = input("Enter word: ") #begin: user input
+
+output = translate(word) #function call
+
+if type(output) == list: #how to deal with >1 definition returned as list. Use type() in debugging!
+    for item in output:
+        print(item)
+else:
+    print(output)
