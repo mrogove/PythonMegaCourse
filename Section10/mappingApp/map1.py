e1becf1ec4d8b6ba1a6d569dc2bc82d7bf05bb90
mrogove <mrogove@gmail.com>
2018-01-16 18:39:21 -0700

    Referencing Volcanoes text file to get coordinates; use loop to assign markers.

--- Section10/mappingApp/map1.py
+++ Section10/mappingApp/map1.py
@@ -1,12 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map([39.721901, -104.994010], zoom_start=5, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.Marker(location=[39.721901,-104.994010],popup="Productive Denver Location",icon=folium.Icon(color='green')))
 fg.add_child(folium.Marker(location=[40.3094215,-105.6708506],popup="White Walkers spotted here",icon=folium.Icon(color='blue')))
 
-for coordinates in [[38.2,-99.1],[39.2,-97.1]]:
-    fg.add_child(folium.Marker(location=coordinates,popup="This is a marker",icon=folium.Icon(color='red')))
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=(lt,ln),popup="This is a VOLCANO",icon=folium.Icon(color='red')))
 
 
 map.add_child(fg)
