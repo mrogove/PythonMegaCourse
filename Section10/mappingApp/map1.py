64d34fdcb69393399782d028d75589a0337d2e47
mrogove <mrogove@gmail.com>
2018-01-17 11:53:33 -0700

    Adding in elevation; using html parsing to avoid issues with blank strings.

--- Section10/mappingApp/map1.py
+++ Section10/mappingApp/map1.py
@@ -4,6 +4,7 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map([39.721901, -104.994010], zoom_start=5, tiles="Mapbox Bright")
 
@@ -11,8 +12,10 @@ fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.Marker(location=[39.721901,-104.994010],popup="Productive Denver Location",icon=folium.Icon(color='green')))
 fg.add_child(folium.Marker(location=[40.3094215,-105.6708506],popup="White Walkers spotted here",icon=folium.Icon(color='blue')))
 
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=(lt,ln),popup="This is a VOLCANO",icon=folium.Icon(color='red')))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln]
+                              ,popup=folium.Popup(str(el), parse_html=True)
+                              ,icon=folium.Icon(color='red')))
 
 
 map.add_child(fg)
