44affc2d2ebba31017e5c219620f460c85a20691
mrogove <mrogove@gmail.com>
2018-01-17 18:51:28 -0700

    Assigned arbitrary color fill for various populations

--- Section10/mappingApp/map2.py
+++ Section10/mappingApp/map2.py
@@ -26,7 +26,8 @@ for lt,ln,el in zip(lat,lon,elev):
                               ,radius=6,color='grey',fill_opacity=0.7, fill=True))
 
 fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read()
-                                    ,style_function=lambda x: {'fillColor':'yellow'}))
+                                    ,style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<10000000
+                                                                else 'orange' if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
 
 
 map.add_child(fg)
