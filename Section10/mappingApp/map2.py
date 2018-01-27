d5218ff20b2a217fca8623a74ef9046d3056479c
mrogove <mrogove@gmail.com>
2018-01-27 00:13:18 -0500

    Adding control layer; removing volcano group.

--- Section10/mappingApp/map2.py
+++ Section10/mappingApp/map2.py
@@ -9,26 +9,14 @@ elev = list(data["ELEV"])
 #init and center map
 map = folium.Map([39.721901, -104.994010], zoom_start=5, tiles="Mapbox Bright")
 
-def get_color(elevation):
-    if elevation < 1000:
-        return('lightgreen')
-    elif elevation < 3000:
-        return('orange')
-    else:
-        return('darkred')
+fgp = folium.FeatureGroup(name="Population")
 
-fg = folium.FeatureGroup(name="My Map")
-
-for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln]
-                              ,popup=folium.Popup(str(el)+" meters", parse_html=True)
-                              ,fill_color=get_color(el)
-                              ,radius=6,color='grey',fill_opacity=0.7, fill=True))
-
-fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read()
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read()
                                     ,style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<10000000
                                                                 else 'orange' if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
 
 
-map.add_child(fg)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 map.save("Map2.html")
